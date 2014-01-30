28d1b521398a5bfe6023304fe9c4b981e5ca0d2c
rentzsch <jwr.git@redshed.net>
2014-01-30 11:33:36 -0600

    Initial Commit

---
+++ xib2pdflabels/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  xib2pdflabels
+//
+//  Created by Wolf Rentzsch on 1/30/14.
+//  Copyright (c) 2014 Jonathan 'Wolf' Rentzsch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
